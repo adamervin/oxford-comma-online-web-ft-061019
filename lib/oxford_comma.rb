077ef17f7c35d9e6efe8296490d44428f779b001
Adam Ervin <adamjervin@gmail.com>
2019-06-11 19:42:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ if array.length == 1
  elsif array.length == 2
  return array.join(" and ")
  
- elsif
-new_last_array_item = "and #{array[-1]}"
+ elsif array.length >= 3
+    new_last_array_item = "and #{array[-1]}"
     array.pop
     array.push(new_last_array_item)
     return array.join(", ")
